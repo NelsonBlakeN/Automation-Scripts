d9b74753c6eb71dbfe5bda8a47b23109590a43f4
NelsonBlakeN <nelson.blaken@gmail.com>
2018-05-22 23:12:59 -0500

    Adding script to update pip

---
+++ upgrade_pip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+date '+[ %m/%d/%Y %H:%M:%S ]'
+
+envs=$HOME/Py3Envs
+
+# Change directory to venvs
+cd $envs
+
+# For each folder in dir, check for .pyenv
+for env in */; do
+    echo "* Updating pip for $env:"
+
+    # Activate env
+    source $envs/$env/bin/activate
+
+    # Upgrade pip
+    pip install --upgrade pip
+done
+
+echo "Complete."
+echo
