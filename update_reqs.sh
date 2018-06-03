f9e9e5d742d04d9ac1002b68b8a9c79e1004dea5
NelsonBlakeN <nelson.blaken@gmail.com>
2018-06-03 16:55:08 -0700

    Complete prototype of pip requirements update script

--- update_reqs.sh
+++ update_reqs.sh
@@ -2,7 +2,39 @@
 
 date '+[ %m/%d/%Y %H:%M:%S ]'
 
-echo "Updating requirements for all projects"
+echo "Updating pip requirements for all projects"
 
-# Obtain list of outdated packages
-$outdated_list=$(pip list --outdated | awk '!/Package|----/ { print $1}')
+# Switch to dev directory
+cd /home/blake/Dropbox/DevWork/
+
+# Loop through each project
+for proj in */; do
+    # Switch to current project
+    cd $proj
+
+    # If $proj has python env, continue
+    if [[ -f .pyenv ]]; then
+        echo Updating $proj
+
+        # Obtain env name and activate
+        env=$(head -n 1 .pyenv)
+        echo Activating $env
+        source /home/blake/Py3Envs/$env/bin/activate
+
+        # Obtain list of outdated packages and update
+        outdated_list=$(pip list --outdated | awk '!/Package|----/ { print $1}')
+        pip install -U $outdated_list
+
+        # Update requirements.txt
+        pip freeze > requirements.txt
+
+        # Clean up
+        deactivate
+    fi
+
+    cd ..
+done
+
+echo Complete.
+echo
+
