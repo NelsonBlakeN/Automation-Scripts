4d924f9907774d6dcdec02ed99722ff68bb1416a
NelsonBlakeN <nelson.blaken@gmail.com>
2018-06-29 18:06:32 -0700

    Full functionality exists and formatting should be correct.

--- cleanup_review/cleanup_review.py
+++ cleanup_review/cleanup_review.py
@@ -21,33 +21,27 @@ if num_downloads is not 0:
     downloads = sys.argv[DL_LIST]
     DL_CONTENT = DL_CONTENT.format(num_downloads, downloads)
 else:
-    DL_CONTENT = DL_CONTENT.format("", "")
-
-print("Downloads")
-print(num_downloads)
-print(downloads)
-print(DL_CONTENT)
+    DL_CONTENT = ""
 
 if sys.argv[TRASH_NUM] is not 0:
     trash = sys.argv[TRASH_LIST]
-    TRASH_CONTENT.format(num_trash, trash)
+    TRASH_CONTENT = TRASH_CONTENT.format(num_trash, trash)
 else:
-    TRASH_CONTENT.format("", "")
+    TRASH_CONTENT = ""
 
-print("Trash")
-print(num_trash)
-print(trash)
+if downloads is not None or trash is not None:
+    try:
+        EMAIL_CONTENT.format(DL_CONTENT, TRASH_CONTENT)
 
-#if downloads is not None and trash is not None:
-#    EMAIL_CONTENT.format(DL_CONTENT, TRASH_CONTENT)
-#
-#    msg = MIMEMultipart("alternative", None, [MIMEText(EMAIL_CONTENT, 'html')])
-#    msg['Subject'] = "Cleanup Review"
-#    msg['From'] = FROM
-#    msg['TO'] = TO
-#    server = smtplib.SMTP(SERVER)
-#    server.ehlo()
-#    server.starttls()
-#    server.login(TO, PASSWORD)
-#    server.sendmail(FROM, TO, msg.as_string())
-#    server.quit()
+        msg = MIMEMultipart("alternative", None, [MIMEText(EMAIL_CONTENT, 'html')])
+        msg['Subject'] = "Cleanup Review"
+        msg['From'] = FROM
+        msg['TO'] = TO
+        server = smtplib.SMTP(SERVER)
+        server.ehlo()
+        server.starttls()
+        server.login(TO, PASSWORD)
+        server.sendmail(FROM, TO, msg.as_string())
+        server.quit()
+    except Exception as e:
+        print("Email failed; try again later.")
--- cleanup_review/cleanup_review.sh
+++ cleanup_review/cleanup_review.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 date '+[ %m/%d/%Y %H:%M:%S ]'
-echo Reviewing old files...
+echo -n "* Reviewing old files..."
 
 MAIL=/home/blake/scripts/Automation/cleanup_review/cleanup_review.py
 
@@ -12,24 +12,23 @@ num_downloads="$(find /home/blake/Downloads/ -mtime +19 -print | wc -l)"
 # Count number of files in trash
 trash="$(find /home/blake/.local/share/Trash/* -mtime +19 -print)"
 num_trash="$(find /home/blake/.local/share/Trash/* -mtime +19 -print | wc -l)"
+echo "complete."
 
-echo
-echo Running python script...
+echo -n "* Running python script..."
 # Pass all data into the python script, which will send the notification
 $MAIL $num_downloads "$downloads" $num_trash "$trash"
+echo "complete."
+
+if [[ $num_downloads -ne 0 ]]; then
+   echo "* Found $num_downloads impending Downloads deletion(s): "
+   echo "$downloads" | awk '{print "\t"$0 }'
+   echo
+fi
+
+if [[ $num_trash -ne 0 ]]; then
+   echo "* Found $num_trash impending Trash deletion(s): "
+   echo "$trash" | awk '{print "\t"$0 }'
+fi
+
 echo Complete.
 echo
-
-#if [[ $num_downloads -ne 0 ]]; then
-#    echo "* Found $num_downloads impending Downloads deletion(s): "
-#    echo "$downloads" | awk '{print "\t"$0 }'
-#    echo
-#fi
-
-#if [[ $num_trash -ne 0 ]]; then
-#    echo "* Found $num_trash impending Trash deletion(s): "
-#    echo "$trash" | awk '{print "\t"$0 }'
-#fi
-
-#echo Complete.
-#echo
