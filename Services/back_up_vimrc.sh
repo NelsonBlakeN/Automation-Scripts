0e1b9f1c5ce3296a1ab1cb853d8c0b1ee371cd72
NelsonBlakeN <nelson.blaken@gmail.com>
2018-08-18 19:20:13 -0700

    Rename folder for performance scripts

---
+++ Services/back_up_vimrc.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+# Back up vimrc file
+
+date '+[ %m/%d/%Y %H:%M:%S ]'
+echo "* Updating vimrc backup."
+echo -n "* Calculating checksums..."
+
+home=/home/blake
+
+vimrc_backup=$(md5sum $home/Dropbox/DevWork/vimrc/.vimrc | awk '{print $1}')
+vimrc_current=$(md5sum $home/.vimrc | awk '{print $1}')
+echo "done."
+
+echo "* Comparing..."
+if [[ $vimrc_backup -ne $vimrc_current ]]; then
+    echo "* Current vimrc file is different than the backup."
+    echo -n "* Syncing..."
+    cp $home/.vimrc $home/Dropbox/DevWork/vimrc/
+    echo "done."
+else
+    echo "* Backup is up to date."
+fi
+
+echo "* Complete."
+echo
---
+++ Services/update_reqs.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+date '+[ %m/%d/%Y %H:%M:%S ]'
+
+echo "* Updating pip requirements for all projects"
+echo "----------------------------"
+
+# Switch to dev directory
+cd /home/blake/Dropbox/DevWork/
+
+# Loop through each project
+for proj in */; do
+    # Switch to current project
+    cd "$proj"
+
+    # If $proj has python env, continue
+    if [[ -f .pyenv ]]; then
+        echo "* Updating $proj"
+
+        # Obtain env name and activate
+        env=$(head -n 1 .pyenv)
+        echo "* Activating $env"
+        source /home/blake/Py3Envs/$env/bin/activate
+
+        # Obtain list of outdated packages and update
+        echo "* Updating outdated packages"
+        outdated_list=$(pip list --outdated | awk '!/Package|----/ { print $1}')
+        if [[ -z "$outdated_list" ]]; then
+            echo "* Nothing to update."
+        else
+            pip install -U $outdated_list
+
+            # Update requirements.txt
+            echo "* Updating requirements file"
+            pip freeze > requirements.txt
+
+            echo "* Committing changes"
+            git add requirements.txt
+            git -c user.name='NelsonBlakeN' -c user.email='nelson.blaken@gmail.com' commit -m "Committing requirements changes. This is an automated command."
+            echo "* Pushing to remote repo"
+            git push
+        fi
+
+        # Clean up
+        deactivate
+
+        echo "* Finished with $proj ($env)"
+        echo "----------------------------"
+    fi
+
+    cd ..
+done
+
+echo Complete.
+echo
+
