39e4a742aebc7fb9d4e888006cf1f6d3bf7a6f5f
NelsonBlakeN <nelson.blaken@gmail.com>
2020-07-14 23:31:00 -0700

    Updating Logger based on functions now implemented in the Log

--- Logger.py
+++ Logger.py
@@ -1,13 +1,13 @@
 import os
 import logging
-
+from Log import TextLog
 
 class Logger(object):
-    def __init__(self, log_name=None):
+    def __init__(self, log_name=None, log_type:str=None):
         self.logs = []
         if log_name:
             self.LOG_NAME = log_name
-            self.add_log(self.LOG_NAME)
+            self.add_log(self.LOG_NAME, log_type)
 
     def log(self, message):
         for log in self.logs:
@@ -21,43 +21,17 @@ class Logger(object):
         for log in self.logs:
             log.warning(message)
 
-    def add_log(self, name):
+    def add_log(self, name: str, log_type: str, log_location: str=None):
         """
         Add a new log object for tracking and writing.
         """
         if not name:
-            raise Exception("Filename is required.")
+            raise Exception("Log name is required.")
+        if not log_type:
+            raise Exception("Log type is required.")
 
         # Verify that log location is available
         # before creating the log config
-        self.__verify_log_location(name)
-        log = self.__create_log(name.split(".")[0], name)
+        print(TextLog)
+        log = TextLog(name, log_location)
         self.logs.append(log)
-
-    def __verify_log_location(self, file_path):
-        """
-        Verify if the location of the given file exists.
-        If the location does not exist, it will be created recursively.
-        """
-        items: list = file_path.split("/")
-        items.pop(-1)
-        folder_path = os.path.expanduser(items.pop(0) + '/')
-        for item in items:
-            folder_path += item + "/"
-            if not os.path.exists(folder_path):
-                os.mkdir(folder_path)
-
-    def __create_log(self, name, logfile, default_level=logging.INFO):
-        """
-        Creates a log using the given name, location, and level
-        TODO: Utilize a factory that generates the right type of log (text file, database, cloud, etc)
-        """
-
-        handler = logging.Handler(logfile)
-        # TODO: Create custom formatter
-
-        log = logging.getLogger(name)
-        log.setLevel(default_level)
-        log.addHandler(handler)
-
-        return log
